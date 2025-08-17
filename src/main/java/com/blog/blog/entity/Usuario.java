e7e3b9da8f925df9de8aaf41baeaf5258965a73a
kazaum <kauan.pinheiro.1502@gmail.com>
2025-08-17 15:17:03 -0300

    data alteracao adicionada a usuario

--- src/main/java/com/blog/blog/entity/Usuario.java
+++ src/main/java/com/blog/blog/entity/Usuario.java
@@ -3,6 +3,7 @@ package com.blog.blog.entity;
 import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -37,4 +38,7 @@ public class Usuario {
     @Column(updatable = false)
     @CreationTimestamp
     private LocalDateTime dataCriacao;
+
+    @UpdateTimestamp
+    private LocalDateTime dataAlteracao;
 }
