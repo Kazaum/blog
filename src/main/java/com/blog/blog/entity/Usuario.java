3461f1b20efa58d2a42540900e9d4662089ae197
kazaum <kauan.pinheiro.1502@gmail.com>
2025-08-17 14:23:44 -0300

    entidade e repositorio usuario criadas

---
+++ src/main/java/com/blog/blog/entity/Usuario.java
@@ -0,0 +1,34 @@
+package com.blog.blog.entity;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+    
+    private String dataCriacao;
+}
