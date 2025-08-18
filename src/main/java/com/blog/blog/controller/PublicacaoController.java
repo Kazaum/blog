9c78a7038fdc6154a68ec77cdafe71a49fbfe6f1
kazaum <kauan.pinheiro.1502@gmail.com>
2025-08-17 22:33:07 -0300

    att de algumas classes

--- src/main/java/com/blog/blog/controller/PublicacaoController.java
+++ src/main/java/com/blog/blog/controller/PublicacaoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.blog.blog.entity.Publicacao;
@@ -22,27 +23,34 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class PublicacaoController {
     private final PublicacaoService service;
 
-    @PostMapping("/create")
+    @PostMapping()
     public ResponseEntity<?> create(@RequestBody Publicacao publicacao){
         return ResponseEntity.ok(service.create(publicacao));
     }
 
-    @GetMapping("/list/all")
-    public ResponseEntity<?> listAll(){
-        return ResponseEntity.ok(service.listAll());
+    @GetMapping("/all")
+    public ResponseEntity<?> listAll(
+        @RequestParam(defaultValue = "0") int paginaInicial,
+        @RequestParam(defaultValue = "10") int tamanhoPagina)
+    {
+        return ResponseEntity.ok(service.listarPaginado(paginaInicial, tamanhoPagina));
     }
 
-    @GetMapping("/list/by/user/{userId}")
-    public ResponseEntity<?> listByUserId(@PathVariable("userId") Long userId){
-        return ResponseEntity.ok(service.listByUserId(userId));
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<?> listByUserId(
+        @PathVariable("userId") Long userId,
+        @RequestParam(defaultValue = "0") int paginaInicial,
+        @RequestParam(defaultValue = "10") int tamanhoPagina)
+    {
+        return ResponseEntity.ok(service.listarPorUsuario(userId, paginaInicial, tamanhoPagina));
     }
 
-    @PutMapping("/update")
+    @PutMapping()
     public ResponseEntity<?> update(@RequestBody Publicacao publicacao) {
         return ResponseEntity.ok(service.update(publicacao));
     }
 
-    @DeleteMapping("delete/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") Long id){
         return ResponseEntity.ok(service.delete(id));
     }
